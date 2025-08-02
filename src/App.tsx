2e17a7a52107ca7e6e5ebe52f6c0a7abe3e905f8
kirankrndev <kiran.2022@jec.ac.in>
2025-08-02 14:28:39 +0530

    frontend changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRouter from "./router/AppRouter";
+
+function App() {
+  return (
+    <div className="w-screen min-h-screen m-0 p-0 text-center">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
