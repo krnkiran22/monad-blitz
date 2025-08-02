63a743c25031d11e30e4c6ce6042bc6e53f06336
kirankrndev <kiran.2022@jec.ac.in>
2025-08-02 15:46:11 +0530

    app router removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import AppRouter from "./router/AppRouter";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DisputePage from './components/DisputePage';
+import ResolutionPage from './pages/ResolutionPage';
+import HelpPage from './pages/HelpPage';
+import Home from './pages/HomePage';
 
 function App() {
   return (
     <div className="w-screen min-h-screen m-0 p-0 text-center">
-      <AppRouter />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/disputes" element={<DisputePage />} />
+          <Route path="/resolution/:id" element={<ResolutionPage />} />
+          <Route path="/help" element={<HelpPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,16 +4,7 @@ const Home = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       {/* Main Spline component */}
-      <Spline 
-        scene="https://prod.spline.design/MaDCOHnRuptcKeiK/scene.splinecode"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%'
-        }}
-      />
+      <Spline scene="https://prod.spline.design/MaDCOHnRuptcKeiK/scene.splinecode" />
     </div>
   );
 };
